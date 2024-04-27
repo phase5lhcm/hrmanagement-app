6733d61b2b505003099bc520a53f1842e22c87d6
cmaynard <chrissy.swe.maynard@gmail.com>
2024-04-27 19:09:28 -0400

    Add custom avatar for dynamic user data per user

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,7 +1,11 @@
 import { Popover, Button } from "antd";
 import React from "react";
+import CustomAvatar from "../custom-avatar";
+import { useGetIdentity } from "@refinedev/core";
+import type { User } from "@/grapql/schema.types";
 
 const CurrentUser = () => {
+  const { data: user } = useGetIdentity<User>();
   return (
     <>
       <Popover
@@ -10,7 +14,7 @@ const CurrentUser = () => {
         overlayInnerStyle={{ padding: 0 }}
         overlayStyle={{ zIndex: 999 }}
       >
-        Test
+        <CustomAvatar name="" />
       </Popover>
     </>
   );
