4902faddded463e701cdf7e4a043560afe63f08e
cmaynard <chrissy.swe.maynard@gmail.com>
2024-04-27 14:28:24 -0400

    Add wrapper class for custom fetchwrapper

---
+++ src/providers/data/fetch-wrapper.ts
@@ -0,0 +1,45 @@
+import { GraphQLFormattedError } from "graphql";
+
+type Error = {
+  message: string;
+  statusCode: string;
+};
+
+const customFetch = async (url: string, options: RequestInit) => {
+  const accessToken = localStorage.getItem("access_token");
+  const headers = options.headers as Record<string, string>;
+  return await fetch(url, {
+    ...options,
+    headers: {
+      ...headers,
+      Authorization: headers?.AuthorizationL || `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+      "Apollo-Require-Priflight": "true",
+    },
+  });
+};
+
+const getGraphQLErrors = (
+  body: Record<"errors", GraphQLFormattedError[] | undefined>
+): Error | null => {
+  if (!body) {
+    return {
+      message: "Unknown Error",
+      statusCode: "INTERNAL_SERVER_ERROR",
+    };
+  }
+
+  if ("errors" in body) {
+    const errors = body?.errors;
+
+    // turn all messages into one
+    const messages = errors?.map((error) => error?.message)?.join("");
+    const code = errors?.[0]?.extensions.code;
+
+    return {
+      message: messages || JSON.stringify(errors),
+      statusCode: code || 500,
+    };
+  }
+  return null;
+};
